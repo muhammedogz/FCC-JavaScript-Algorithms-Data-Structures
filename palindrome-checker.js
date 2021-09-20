0b36ee5b546a7a4dcd2c4626ce28332a668148ec
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-09-20 23:53:02 +0300

    Add fibonacci func

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,10 +1,13 @@
-function whatIsInAName(collection, source) {
-    var arr = [];
-    // Only change code below this line
-  
-  
-    // Only change code above this line
-    return arr;
+function sumFibs(num) {
+    let pre = 1;
+    let now = 1;
+    for (let i = 0; i < num; i++) {
+        let fib = pre + now;
+        pre = now;
+        now = fib;
+        console.log(fib);
+    }
+    
 }
   
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+sumFibs(10);
