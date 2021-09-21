64fb0c3a479079125ccf1ddce167449ff87392fe
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-09-21 05:49:07 +0300

    Complete roman-number converter

--- roman-number.js
+++ roman-number.js
@@ -1,5 +1,57 @@
+const romanLiterals = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1:"I"
+};
+
 function convertToRoman(num) {
-    return num;
+    let str = "";
+    // get all keys of object since JavaScript objects does not have specific order. 
+    const romanObjKeys = Object.keys(romanLiterals).reverse().map(Number);
+
+    // add values and decrement
+    while (num > 0) {
+        for (let i = 0; i < romanObjKeys.length; i++) {
+            const val = romanObjKeys[i];
+            if (val <= num) {
+                str += romanLiterals[val];
+                num -= val;
+                break;   
+            }
+        }
+    }
+    return str;
 }
-   
-convertToRoman(36);
+
+console.log(convertToRoman(4));
+console.log(convertToRoman(150));
+console.log(convertToRoman(14));
+console.log(convertToRoman(27));
+console.log(convertToRoman(45));
+console.log(convertToRoman(46));
+console.log(convertToRoman(44));
+console.log(convertToRoman(796));
+console.log(convertToRoman(3999));
+
+
+
+
+/* ROMAN NUMBER TIP */
+
+/* MeDiCaL XaVIar */
+/* 
+M === 1000 - D === 500
+C === 100  - L === 50
+X === 10   - V === 5
+I === 1
+*/
