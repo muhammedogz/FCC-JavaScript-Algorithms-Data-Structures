659870fb050c17a6c6475de3b78e4e76698a9b5e
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-09-21 05:00:31 +0300

    Add tests

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,10 +1,11 @@
 function palindrome(str) {
-    const reg = /[\W]/g;
+    const reg = /[_\W]/g;
     str = str.replace(reg, "").toLowerCase();
-
-    return str.toLowerCase() === str.split(" ").reverse().join(" ");
+    return str.toLowerCase() === str.split("").reverse().join("");
 }
   
-  
-  
+console.log(palindrome("eye"));
 console.log(palindrome("_eye"));
+console.log(palindrome("race car"));
+console.log(palindrome("not a palindrome"));
+console.log(palindrome("almostomla"));
