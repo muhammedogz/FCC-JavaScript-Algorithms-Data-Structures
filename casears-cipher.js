4f31322778b5d78d20c5b9814f2bee33d6d4f153
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-09-21 07:24:17 +0300

    Add solution

--- casears-cipher.js
+++ casears-cipher.js
@@ -1,6 +1,25 @@
 function rot13(str) {
-
-    return str;
+    /**
+     * convert to array
+     * iterate all values
+     * check if item is a letter
+     * if valid letter, than add 13 (ceaser code)
+     * if exceed to value 90, 
+     * convert to starting position
+     */
+    return str.split("").map(letter => {
+        const charCode = letter.charCodeAt();
+        if (charCode >= 65 && charCode <= 90) {
+            let newCharCode = charCode + 13;
+            if (newCharCode > 90) 
+                newCharCode = newCharCode % 90 + 64;
+            return String.fromCharCode(newCharCode);
+        } else 
+            return letter;
+    }).join("");
 }
   
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR PBQR PNZC")); //  FREE CODE CAMP
+console.log(rot13("SERR CVMMN!")); // FREE PIZZA
+console.log(rot13("SERR YBIR?")); // FREE LOVE?
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")); // THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
